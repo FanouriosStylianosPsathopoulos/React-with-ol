3ab906b7c19e194aa8f338dd5fd7004c35a6f4e1
fanmfh <sdi1400320@di.uoa.gr>
2022-03-03 02:37:48 +0200

    First Screen of App . Two buttons leading to a different html

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import './Map.css';
+import styles from  './mystyle.module.css';
+import Render_Map from './Map.js';
+import {Make_Form,Pass_Address} from './Form.js';
+import {Renter,Landlord} from './Buttons.js';
+import First_Screen from './first_screen.js' ;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <First_Screen/>
     </div>
+    
   );
 }
 
